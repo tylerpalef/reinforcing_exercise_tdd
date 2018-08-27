cc9e130d2b15ba0347cd4157f5928454414e7129
“Tyler <tylerpalef@gmail.com>
2018-08-27 13:39:06 -0400

    finished calculator exercise

---
+++ calculator.rb
@@ -0,0 +1,14 @@
+def add(num1, num2)
+  sum = num1 + num2
+  return sum
+end
+
+def subtract(num1, num2)
+  product = num1 - num2
+  return product
+end
+
+def sum(x)
+  product = x.sum
+  return product
+end
